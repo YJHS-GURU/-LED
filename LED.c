42b26fcac891790c519c6363c61176f76f7155bf
Heram06 <76657709+jy852@users.noreply.github.com>
2023-04-14 02:41:45 +0900

    Update LED.c

--- LED.c
+++ LED.c
@@ -1,12 +1,15 @@
-#define LED_PIN 13   // LED가 연결된 핀
-
 void setup() {
-  pinMode(LED_PIN, OUTPUT);   // LED 핀을 출력으로 설정
+  // put your setup code here, to run once:
+  Serial.begin(19200); 
+  pinMode(13, OUTPUT); //LED의 긴다리가 연결된 13번 핀을 출력으로 설정
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);   // LED 켜기
-  delay(1000);   // 1초 대기
-  digitalWrite(LED_PIN, LOW);   // LED 끄기
-  delay(1000);   // 1초 대기
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH); // 13번 핀에 GND와 5V의 전압을 걸어줌
+  Serial.println("5v");
+  delay(1000); // 1초 대기
+  digitalWrite(13, LOW); // 13번 핀에 GND와 0v의 전압을 걸어줌
+  Serial.println("0v");
+  delay(1000); // 1초대기
 }
